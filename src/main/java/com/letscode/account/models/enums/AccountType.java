3ad4f672a358e0b737afbd3de59253a4abd4efbd
Gustavo <gusfeh07@gmail.com>
2022-04-27 15:37:27 -0300

    Initial commit

---
+++ src/main/java/com/letscode/account/models/enums/AccountType.java
@@ -0,0 +1,11 @@
+package com.letscode.account.models.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum AccountType {
+    PF,
+    PJ
+}
